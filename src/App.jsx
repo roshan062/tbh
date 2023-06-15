1d208da7ef657ef9e56ba2a8e0fc184f1897689d
Roshan Prasad <roshanprasad062@gmail.com>
2023-06-15 14:20:00 +0530

    fifth article complete

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ function App() {
     <>
       <Navbar />
       {/* <ArticleOne /> */}
-      <ArticleThree />
-      {/* <ArticleFive /> */}
+      {/* <ArticleThree /> */}
+      <ArticleFive />
       <Footer />
     </>
   )
--- src/pages/ArticleFive.jsx
+++ src/pages/ArticleFive.jsx
@@ -39,6 +39,8 @@ const ArticleFive = () => {
         }
     }
 
+    // const paragraphs = data.description.split('<p>').filter(Boolean);
+
     return (<>
         {data ? (<>
 
@@ -59,17 +61,15 @@ const ArticleFive = () => {
             ) : (
                 <p>Loading data...</p>
             )}
+
+            {/* {paragraphs.map((paragraph, index) => (
+                <div
+                    key={index}
+                    dangerouslySetInnerHTML={{ __html: `<p>${paragraph}</p><br>` }}
+                />
+            ))} */}
+
         </section>
-
-        {/* <section className='prologue'>
-
-            <h2 className='l-arrow'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 512 512" id="left-arrow"><path d="M189.8 349.7c3.1-3.1 3-8 0-11.3L123.4 264H408c4.4 
-        0 8-3.6 8-8s-3.6-8-8-8H123.4l66.3-74.4c2.9-3.4 3.2-8.1.1-11.2-3.1-3.1-8.5-3.3-11.4-.1 0 0-79.2 87-80 88S96 253.
-        1 96 256s1.6 4.9 2.4 5.7 80 88 80 88c1.5 1.5 3.6 2.3 5.7 2.3s4.1-.8 5.7-2.3z"></path></svg>
-
-                Back To News</h2>
-        </section> */}
     </>
     )
 }
