4e5991ec7f5d004d0a2451452a40e27342cd6b9a
Roshan Prasad <roshanprasad062@gmail.com>
2023-09-02 00:37:40 +0530

    home page update 2

--- src/components/ShowMore.jsx
+++ src/components/ShowMore.jsx
@@ -1,21 +1,21 @@
-// import React from 'react';
-// import styles from './ShowMore.module.css';
+import React from 'react';
+import styles from './ShowMore.module.css';
 
-// function ShowMore() {
-//     return (
-//         <div className={styles.iconContainer}>
-//             <svg xmlns="http://www.w3.org/2000/svg" width="60" height="61.979" viewBox="0 0 60 61.979">
-//                 <g id="PLUS" transform="translate(-897.5 -3715.5)">
-//                     <path id="Line_477" data-name="Line 477" d="M60,2H0V-2H60Z" transform="translate(897.5 3745.5)" fill="#fff" />
-//                     <path id="Line_478" data-name="Line 478" d="M60,2H0V-2H60Z" transform="translate(927.5 3715.5) rotate(90)" fill="#fff" />
-//                 </g>
-//             </svg>
-
-
+function ShowMore() {
+    return (
+        <div className={styles.iconContainer}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="60" height="61.979" viewBox="0 0 60 61.979">
+                <g id="PLUS" transform="translate(-897.5 -3715.5)">
+                    <path id="Line_477" data-name="Line 477" d="M60,2H0V-2H60Z" transform="translate(897.5 3745.5)" fill="#fff" />
+                    <path id="Line_478" data-name="Line 478" d="M60,2H0V-2H60Z" transform="translate(927.5 3715.5) rotate(90)" fill="#fff" />
+                </g>
+            </svg>
 
 
-//         </div>
-//     );
-// }
 
-// export default ShowMore;
+
+        </div>
+    );
+}
+
+export default ShowMore;
